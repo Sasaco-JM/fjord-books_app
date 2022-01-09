588540d08f002b8ab1119d5da8aef249e2033982
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2022-01-09 16:11:01 +0900

    rootを本一覧画面に設定、不要になったコントローラーを削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show]
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  root to: 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

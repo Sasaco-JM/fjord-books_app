00c5dc650b0f913c7a45a292a9257a211feaf080
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2022-02-20 18:59:56 +0900

    日報の投稿者表示機能追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  def name_or_email
+    user = User.find(user_id)
+    user.name.presence || user.email
+  end
 end

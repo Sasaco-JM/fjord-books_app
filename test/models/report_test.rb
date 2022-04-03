c9d02cfe4f21f806a7e2fcabe97ec7b981f71ba7
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2022-04-03 11:26:51 +0900

    テストコード記述

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    me_report = me.reports.create!(title: 'report_title', content: 'report_content')
+    assert me_report.editable?(me)
+    assert_not me_report.editable?(she)
+  end
+
+  test '#created_on' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+
+    me_report = me.reports.create!(title: 'report_title', content: 'report_content')
+    assert me_report.created_on.to_s
+  end
 end

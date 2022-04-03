a93485d920be1c464bae4e9fddcfd44a19141bae
“sasacoJM” <78665068+Sasaco-JM@users.noreply.github.com>
2022-04-03 18:17:18 +0900

    follow,following?,followed_by?メソッドのテストを統合

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,13 +11,17 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 'test name', user.name_or_email
   end
 
-  test '#follow' do
+  test '#follow_following?_followed_by?' do
     me = User.create!(email: 'me@example.com', password: 'password')
     she = User.create!(email: 'she@example.com', password: 'password')
 
     assert_not me.following?(she)
+    assert_not she.followed_by?(me)
+
     me.follow(she)
+
     assert me.following?(she)
+    assert she.followed_by?(me)
   end
 
   test '#unfollow' do
@@ -27,23 +31,5 @@ class UserTest < ActiveSupport::TestCase
     assert_not me.unfollow(she)
     me.follow(she)
     assert me.unfollow(she)
-  end
-
-  test '#following?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not me.following?(she)
-    me.follow(she)
-    assert me.following?(she)
-  end
-
-  test '#followed_by?' do
-    me = User.create!(email: 'me@example.com', password: 'password')
-    she = User.create!(email: 'she@example.com', password: 'password')
-
-    assert_not she.followed_by?(me)
-    me.follow(she)
-    assert she.followed_by?(me)
   end
 end
